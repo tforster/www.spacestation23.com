04ffc5b37e0698dd909a8a8b2fe68de96f78045b
Troy Forster <troy.forster@gmail.com>
2022-04-29 12:36:48 -0400

    - Updated deployment scripts to use the AWS CLI via a Docker run function. - Readded last three episodes accidentally dropped when migrating from Ubuntu 20.04 to 22.04 - Added latest episode

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -1,6 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+####################################################################################################################################
+# prod.sh: Synchronise the built files in /dist to the production bucket and invalidate the CloudFront cache.
+#
+####################################################################################################################################
+set -euo pipefail
 
+# Export .env variables
+set -o allexport
+source .env
+set +o allexport
+
+aws() {
+  docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli $@
+}
+
+# Copy all files that have changed since the last time sync ran 
 joy aws-cli s3 sync ./dist s3://www.spacestation23.com --cache-control max-age=31536000 --acl public-read --profile ss23
+
+# Separate copy for index in order that we may also force its content-type to text/html
 joy aws-cli s3 cp s3://www.spacestation23.com/index s3://www.spacestation23.com/index --cache-control max-age=31536000 --content-type text/html --acl public-read --profile ss23
+
+# Force robots.txt to allow indexing (denied by default so that non-production sites do not get indexed)
 echo "User-agent: *\nAllow: /" | joy aws-cli s3 cp - s3://www.spacestation23.com/robots.txt --cache-control max-age=31536000 --content-type text/plain --acl public-read --profile ss23
+
+# Invalidate the CloudFront cache
 joy aws-cli cloudfront create-invalidation --distribution-id E2BPBOUKVVHJIF --paths "/index" "/feed.xml" --profile ss23
--- scripts/stage.sh
+++ scripts/stage.sh
@@ -1,4 +1,21 @@
-#!/bin/sh
+#!/bin/bash
+####################################################################################################################################
+# stage.sh: Synchronise the built files in /dist to the stage bucket
+#
+####################################################################################################################################
+set -euo pipefail
 
-joy aws-cli s3 sync ./dist s3://www.stage.spacestation23.com --cache-control max-age=31536000 --acl public-read --profile ss23
-joy aws-cli s3 cp s3://www.stage.spacestation23.com/index s3://www.stage.spacestation23.com/index --cache-control max-age=31536000 --content-type text/html --acl public-read --profile ss23
+# Export .env variables
+set -o allexport
+source .env
+set +o allexport
+
+aws() {
+  docker run --rm -it -v ~/.aws:/root/.aws -v $(pwd):/aws amazon/aws-cli $@
+}
+
+# Copy all files that have changed since the last time sync ran 
+aws s3 sync ./dist s3://www.stage.spacestation23.com --cache-control max-age=31536000 --acl public-read --profile ss23
+
+# Separate copy for index in order that we may also force its content-type to text/html
+aws s3 cp s3://www.stage.spacestation23.com/index s3://www.stage.spacestation23.com/index --cache-control max-age=31536000 --content-type text/html --acl public-read --profile ss23
