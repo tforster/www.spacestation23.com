37ca88d5de0584e28d0424745fafd4ddb76fbc66
Troy Forster <troy.forster@gmail.com>
2022-05-13 12:30:33 -0400

    Updated episodes

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -15,13 +15,13 @@ aws() {
 }
 
 # Copy all files that have changed since the last time sync ran 
-joy aws-cli s3 sync ./dist s3://www.spacestation23.com --cache-control max-age=31536000 --acl public-read --profile ss23
+aws s3 sync ./dist s3://www.spacestation23.com --cache-control max-age=31536000 --acl public-read --profile ss23
 
 # Separate copy for index in order that we may also force its content-type to text/html
-joy aws-cli s3 cp s3://www.spacestation23.com/index s3://www.spacestation23.com/index --cache-control max-age=31536000 --content-type text/html --acl public-read --profile ss23
+aws s3 cp s3://www.spacestation23.com/index s3://www.spacestation23.com/index --cache-control max-age=31536000 --content-type text/html --acl public-read --profile ss23
 
 # Force robots.txt to allow indexing (denied by default so that non-production sites do not get indexed)
-echo "User-agent: *\nAllow: /" | joy aws-cli s3 cp - s3://www.spacestation23.com/robots.txt --cache-control max-age=31536000 --content-type text/plain --acl public-read --profile ss23
+echo "User-agent: *\nAllow: /" | aws s3 cp - s3://www.spacestation23.com/robots.txt --cache-control max-age=31536000 --content-type text/plain --acl public-read --profile ss23
 
 # Invalidate the CloudFront cache
-joy aws-cli cloudfront create-invalidation --distribution-id E2BPBOUKVVHJIF --paths "/index" "/feed.xml" --profile ss23
+aws cloudfront create-invalidation --distribution-id E2BPBOUKVVHJIF --paths "/index" "/feed.xml" --profile ss23
